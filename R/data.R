5fd95c414acb97b0d07a73d120ecedfc21039500
Tony Fujs <tony.fujs@gmail.com>
2019-05-28 22:42:34 +0200

    document data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,13 @@
 #' sample_input
 #'
-#' A sample dataset to facilitate testing of pocalnet_cl() function
-#'
-#' @country iso3 country codes
-#' @poverty_line Poverty line in PPP
-#' @year Survey year in YYYY format
-#' @ppp PPP exchange rate
-#' @coverage_type Survey coverage
-NULL
+#' A sample dataset to facilitate testing of `povcalnet_cl()`` function
+#' @format A data frame with 5 rows and 5 variables:
+#' \describe{
+#'   \item{country}{iso3 country codes}
+#'   \item{poverty_line}{Poverty line, in PPP}
+#'   \item{year}{Survey year in YYYY format}
+#'   \item{ppp}{PPP exchange rate}
+#'   \item{coverage_type}{Survey coverage}
+#' }
+#' @export
+"sample_input"
